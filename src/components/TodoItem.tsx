f295d17141abe943b6ff724005bed5ea90201650
Ethan Cloin <psnethan@gmail.com>
2022-10-10 18:51:46 -0400

    minor css improvements

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,9 +8,6 @@ interface Props {
 }
 
 const TodoItem = ({ todoData, deleteTodo, onComplete }: Props) => {
-  // should I have a local state variable to track the completed status here?
-  // thinking add a setter up in the App component and pass it down
-
   return (
     <li className="TodoItem">
       {!todoData.isComplete && (
